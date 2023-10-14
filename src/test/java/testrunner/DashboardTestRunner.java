226e8b0f868878a62ade8f8a307e82bab84f505c
Rahat65 <m.rahatdrmc65@gmail.com>
2023-10-14 21:36:54 +0600

    Suite file management, grouping and allure report generate

--- src/test/java/testrunner/DashboardTestRunner.java
+++ src/test/java/testrunner/DashboardTestRunner.java
@@ -3,8 +3,10 @@ package testrunner;
 import com.github.javafaker.Faker;
 import config.EmployeeModel;
 import config.Setup;
+import io.qameta.allure.Allure;
 import org.json.simple.parser.ParseException;
 import org.openqa.selenium.By;
+import org.testng.Assert;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import pages.DashboardPage;
@@ -14,13 +16,33 @@ import utils.Utils;
 import java.io.IOException;
 
 public class DashboardTestRunner extends Setup {
-    @BeforeTest
+    DashboardPage dashboardPage;
+
+    @BeforeTest(groups = "smoke")
     public void login() {
         LoginPage loginPage = new LoginPage(driver);
         loginPage.doLogin("Admin", "admin123");
+        dashboardPage = new DashboardPage(driver);
+        dashboardPage.menuItems.get(1).click(); //click PIM
     }
 
-    @Test(priority = 1)
+    @Test(priority = 1, groups = "smoke", description = "Check if search button is working")
+    public void clickonSearchButton() {
+//        dashboardPage = new DashboardPage(driver);
+//        dashboardPage.menuItems.get(1).click(); //click PIM
+        driver.findElement(By.cssSelector("[type='submit']")).click();
+//        String textActual = driver.findElements(By.className("oxd-text--span")).get(12).getText();
+//        System.out.println(textActual);
+//        String textExpected = "Records Found";
+//        Assert.assertTrue(textActual.contains(textExpected));
+    }
+
+    @Test(priority = 2, groups = "smoke", description = "Check if reset button is working")
+    public void clickonResetButton() {
+        driver.findElement(By.cssSelector("[type='reset']")).click();
+    }
+
+    @Test(priority = 3, description = "Check if new user is created successfully")
     public void createUser() throws IOException, ParseException, InterruptedException {
         DashboardPage dashboardPage = new DashboardPage(driver);
         Faker faker = new Faker();
@@ -43,5 +65,6 @@ public class DashboardTestRunner extends Setup {
         if (textTitleExpected.contains("Personal Details")) {
             Utils.saveEmployeeInfo(model);
         }
+        Allure.description("User created successfully");
     }
 }
